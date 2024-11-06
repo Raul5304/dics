3ba6ac8ea6040156d170041458f1c5421221f20e
Raul5304 <raullecinena@iesrioarba.es>
2024-11-06 10:18:31 +0100

    first commit

---
+++ 0.py
@@ -0,0 +1,20 @@
+#! /C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
+
+
+L = []
+# Una lista es un contenedor de i con la i estructurada como simple sucesion de items a la i se accede por posición (minimo)
+# pos -> valor
+
+D = {}
+# Un diccionario es un contenedor de informacion con la i estructurada como simples parejas k-v desordenadas, a la i se accede por k. (minimo)
+# k -> valor
+
+if __name__ == "__main__":
+    
+    print(D.__dir__)
+    print(D.__dir__())
+    
+    # D.__delattr__()
+    print(D.__class__())
+    
+    # D.__setattr__("color", "rojo")
